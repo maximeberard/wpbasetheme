668498b1c36b2f65d4ab440fae711ee968525967
Maxime Bérard <contact@maximeberard.com>
2020-05-13 13:03:23 +1200

    Changed manifest to fit new realfavicon generator syntax

--- header.php
+++ header.php
@@ -53,7 +53,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/favicons/manifest.json">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/favicons/site.webmanifest">
     <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicons/safari-pinned-tab.svg" color="#ffffff">
     <meta name="theme-color" content="#ffffff">
 
